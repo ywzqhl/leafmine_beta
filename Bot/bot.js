f2b48034710add5eebd82b5bf0a8dceb489350d6
ywzqhl <450099136@qq.com>
2024-05-25 13:40:17 +0800

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,1 +1,15 @@
+const { Telegraf } = require("telegraf");
+const TOKEN = "7041763974:AAHRVMVsFza1HneXfKDifYY2or2ydoX93S8";
+const bot = new Telegraf(TOKEN);
 
+const web_link = "https://leafmine.vercel.app/";
+
+bot.start((ctx) =>
+  ctx.reply("Welcome :)))))", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
+
+bot.launch();
