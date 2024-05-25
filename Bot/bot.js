af6a7459bfc597bc15c786702f0fccb70b72720a
ywzqhl <450099136@qq.com>
2024-05-25 14:31:18 +0800

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "6820674346:AAGXI04qSIvcqeCr_6coRs_rO7tBt1vya48";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://duangkitty.vercel.app/";
+const web_link = "https://frontend-duangkitty.vercel.app/";
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
